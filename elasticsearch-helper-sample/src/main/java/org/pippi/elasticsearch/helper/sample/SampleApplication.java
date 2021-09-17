819272655a9e6f0050e306760321c4ed70968b7b
邓天嘉 <dengtianjia@fiture.com>
2021-09-17 14:26:45 +0800

    调整结构，新增springboot集成测试环境

---
+++ elasticsearch-helper-sample/src/main/java/org/pippi/elasticsearch/helper/sample/SampleApplication.java
@@ -0,0 +1,19 @@
+package org.pippi.elasticsearch.helper.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SampleApplication
+ *
+ * @author johen
+ * @date 2021/9/17
+ */
+@SpringBootApplication
+public class SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+
+}
