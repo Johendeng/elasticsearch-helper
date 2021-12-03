7ac87a8ec2178306894f0fd14ab2c1e19016a7df
JohenTeng <1078481395@qq.com>
2021-12-04 07:34:01 +0800

    新增条件判断

---
+++ elasticsearch-helper-core/src/main/java/org/pippi/elasticsearch/helper/core/EsConditionHandle.java
@@ -0,0 +1,14 @@
+package org.pippi.elasticsearch.helper.core;
+
+/**
+ * project: elasticsearch-helper
+ * package: org.pippi.elasticsearch.helper.core
+ * date:    2021/12/3
+ * developer: JohenTeng
+ * email: 1078481395@qq.com
+ **/
+public interface EsConditionHandle {
+
+    boolean test(Object val);
+
+}
---
+++ elasticsearch-helper-core/src/main/java/org/pippi/elasticsearch/helper/core/beans/annotation/query/EsCondition.java
@@ -0,0 +1,22 @@
+package org.pippi.elasticsearch.helper.core.beans.annotation.query;
+
+import org.pippi.elasticsearch.helper.core.EsConditionHandle;
+
+import java.lang.annotation.*;
+
+/**
+ * project: elasticsearch-helper
+ * package: org.pippi.elasticsearch.helper.core.beans.annotation.query
+ * date:    2021/12/3
+ * developer: JohenTeng
+ * email: 1078481395@qq.com
+ **/
+@Inherited
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EsCondition {
+
+    Class<? extends EsConditionHandle> value();
+
+}
