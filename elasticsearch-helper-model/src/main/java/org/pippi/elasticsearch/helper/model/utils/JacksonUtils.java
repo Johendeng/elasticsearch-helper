f6227240d893f844f387a2021ce7ee59a57c0d19
dengtianjia <1078481395@qq.com>
2024-03-28 15:06:05 +0800

    feat(2.0.0) 移除无用引用

--- elasticsearch-helper-model/src/main/java/org/pippi/elasticsearch/helper/model/utils/JacksonUtils.java
+++ elasticsearch-helper-model/src/main/java/org/pippi/elasticsearch/helper/model/utils/JacksonUtils.java
@@ -19,10 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
-import java.util.List;
 
 /**
  * @author    JohenTeng
